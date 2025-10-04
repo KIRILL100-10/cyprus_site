994c1142d376bb8f1822884f2296a1d32a76a011
KIRILL100-10 <Kirillnadtocaev@gmail.com>
2025-10-04 14:05:48 +0300

    Created templates for the authentication system, updated main templates, and fixed minor bugs

--- cyprus_site/cyprus_site/urls.py
+++ cyprus_site/cyprus_site/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', include('main.urls')),
     path('users/', include('users.urls', namespace='users')),
 ]
