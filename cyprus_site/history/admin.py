d421bde7c22753f8785d33c819be3e014d674621
KIRILL100-10 <Kirillnadtocaev@gmail.com>
2025-10-06 12:11:39 +0300

    Configured admin panel for articles and categories, and applied missing migration

--- cyprus_site/history/admin.py
+++ cyprus_site/history/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Article
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title')
+    list_per_page = 5
+    ordering = ('-created_at',)
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
